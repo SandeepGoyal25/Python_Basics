5a184f64d65ada737c511171aba90895bb8710a8
Sandy <goyalsandy@gmail.com>
2022-03-24 18:58:32 +0530

    commit from line 500 to 600

--- 37_practice.py
+++ 37_practice.py
@@ -562,3 +562,71 @@ a = 100
 b = 200
 if b>a:
     pass
+
+i = 1
+while i < 5:
+  print(i)
+  i += 1
+
+i = 1
+while i < 7:
+  print(i)
+  if i == 4:
+    break
+  i += 1
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+for x in "banana":
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  if x == "banana":
+    break
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x) 
+
+for x in range(5, 100, 10):
+  print(x)
+
+for x in range (7):
+  if x == 3: break
+  print(x)
+else:
+  print("Thus finished!")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+for x in [0, 1, 2]:
+  pass
